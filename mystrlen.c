435ca18298d7a75674e2eb980c4ad5b8473460d4
caleb-sims <caleb.sims@students.fhu.edu>
2026-08-28 16:05:16 -0500

    Create mystrlen.c

---
+++ mystrlen.c
@@ -0,0 +1,19 @@
+
+
+size_t mystrlen(const char *str);
+
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
+        printf("usage: %s <word>\n", argv[0]);
+        return 1;
+    }
+
+
+    
+    return 0;
+}
+
+size_t my_strlen(const char *str) {
+
+}
